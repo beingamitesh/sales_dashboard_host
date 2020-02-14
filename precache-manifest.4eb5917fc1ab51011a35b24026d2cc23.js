a23e2b595bc19f30ac6448f50460f199fbbfebc9
beingamitesh <amitesh.srivastava22@gmail.com>
2020-02-14 13:30:21 +0530

    Committed host

---
+++ precache-manifest.4eb5917fc1ab51011a35b24026d2cc23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0e836f7ebbe36990dd0581bd6ba26e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0921420029986740774c",
+    "url": "/static/css/2.276af938.chunk.css"
+  },
+  {
+    "revision": "cb9ac29e874eb5016802",
+    "url": "/static/css/main.2b422f3e.chunk.css"
+  },
+  {
+    "revision": "0921420029986740774c",
+    "url": "/static/js/2.b260f40b.chunk.js"
+  },
+  {
+    "revision": "f802186dae299887554f1578205d241f",
+    "url": "/static/js/2.b260f40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9ac29e874eb5016802",
+    "url": "/static/js/main.7169a755.chunk.js"
+  },
+  {
+    "revision": "74637e81bc0ab408745d",
+    "url": "/static/js/runtime-main.b086c7c7.js"
+  }
+]);
